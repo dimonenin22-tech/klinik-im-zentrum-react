b0162a15b9b5681873771c9dfce9fc727da61fdc
dimonenin22-tech <dimonenin.22@gmail.com>
2026-09-20 21:34:41 +0300

    deploy: update webhook URL to status-first row-coloring Google Sheets

--- src/lib/googleSheets.ts
+++ src/lib/googleSheets.ts
@@ -1,9 +1,10 @@
 export const DEFAULT_GOOGLE_SHEETS_WEBHOOK_URL =
-  "https://script.google.com/macros/s/AKfycbwIwXUyB0kI-K7T8uB0zxina7m381SHfhZcdK_QJ49haqPA13R3iZcVArX9DSZ8rIDdYA/exec";
+  "https://script.google.com/macros/s/AKfycbxUZ6A2Daz-4VJuJi1J1jSJblCukJR-lBwSOibdFbsWinO-WZhbjnR8QiWCJncbFt7jdQ/exec";
 
 const PREVIOUS_URLS = [
   "https://script.google.com/macros/s/AKfycbz9LswxdNR8nhAegROl_LNY4v1jWFep3Hg2F4cFy5H9WTtCtGYgIN1C2OgawVsMaJ6-/exec",
   "https://script.google.com/macros/s/AKfycbyYrk1u-RaBYQgxy3ecgkg37UrQVmZzXt4uvtrb3LelmzN-Ql632261BcjHl2qjacvV/exec",
+  "https://script.google.com/macros/s/AKfycbwIwXUyB0kI-K7T8uB0zxina7m381SHfhZcdK_QJ49haqPA13R3iZcVArX9DSZ8rIDdYA/exec",
 ];
 
 const STORAGE_KEY = "kiz_google_sheets_webhook_url";
